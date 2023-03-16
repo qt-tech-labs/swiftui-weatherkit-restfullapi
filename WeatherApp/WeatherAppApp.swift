8fe541932f6a38fbc1766df10ef6f2cb6b6c42fd
Nguyen Quy Trung <nguyenquytrungvodich@gmail.com>
2023-03-16 15:29:01 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Trung Nguyễn on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
